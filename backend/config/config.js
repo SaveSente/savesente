1a4981f40176f20849a05833f906df52426800be
Aigo Faith <aigofaith@gmail.com>
2025-07-25 10:35:54 +0300

    load from the env file

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,9 +3,9 @@ import 'dotenv/config'
 
 const config =   {
   "development": {
-    "username": "postgres",
-    "password": "password1",
-    "database": "savesente_dev",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
